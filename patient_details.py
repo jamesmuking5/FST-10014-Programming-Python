95646eca1b7e43fb2175609ef0006a4736a44d3d
Svelte <jamesmuking@gmail.com>
2022-12-06 02:01:16 +0800

    Completed patient details retheme

--- patient_details.py
+++ patient_details.py
@@ -1,4 +1,5 @@
-from tkinter import *
+from customtkinter import *
+import tkinter
 import tkinter.messagebox as MessageBox
 from mysql import *
 import mysql.connector as mysql
@@ -23,9 +24,9 @@ def patient_details_collect(username):
 
 #Patient View
 def pt_details_pt_view(root_home,username):
-    pt_details_pt_view = Toplevel(root_home)
+    pt_details_pt_view = CTkToplevel(root_home)
     pt_details_pt_view.title("MCB: Your details")
-    pt_details_pt_view.geometry("430x350")
+    pt_details_pt_view.geometry("450x670")
 
     #running SQL query in function for patient details
     weird_list_tuple = patient_details_collect(username)
@@ -49,71 +50,71 @@ def pt_details_pt_view(root_home,username):
     patient_emer_contact=patient_details_tuple[15]
     
     #creating labels
-    welcome_text= Label(pt_details_pt_view, font=("Times",12), text="Patient details, "+full_name)
-    ic_label = Label(pt_details_pt_view,text="Identification Card Num.:")
-    blood_pressure_label = Label(pt_details_pt_view,text="Blood Pressure:")
-    spiro_oxy_label = Label(pt_details_pt_view,text="SpO2:")
-    heart_rate_label = Label(pt_details_pt_view,text="Heart Rate:")
-    temperature_label = Label(pt_details_pt_view,text="Body Temperature:")
-    current_disease_label = Label(pt_details_pt_view,text="Current Diseases:")
-    medical_history_label = Label(pt_details_pt_view,text="Medical History:")
-    patient_weight_label = Label(pt_details_pt_view,text="Recent Weight:")
-    patient_height_label = Label(pt_details_pt_view,text="Recent Height:")
-    patient_bmi_label = Label(pt_details_pt_view,text="Recent BMI:")
-    patient_allergies_label = Label(pt_details_pt_view,text="Known Allergies:")
-    patient_contact_label = Label(pt_details_pt_view,text="Patient Contact Num.:")
-    patient_emer_contact_label = Label(pt_details_pt_view,text="Patient Emergency Contact Num.:")
+    welcome_text= CTkLabel(pt_details_pt_view, font=("Gadugi",18), text="Your patient details, "+full_name)
+    ic_label = CTkLabel(pt_details_pt_view,text="Identification Card Num.:")
+    blood_pressure_label = CTkLabel(pt_details_pt_view,text="Blood Pressure:")
+    spiro_oxy_label = CTkLabel(pt_details_pt_view,text="SpO2:")
+    heart_rate_label = CTkLabel(pt_details_pt_view,text="Heart Rate:")
+    temperature_label = CTkLabel(pt_details_pt_view,text="Body Temperature:")
+    current_disease_label = CTkLabel(pt_details_pt_view,text="Current Diseases:")
+    medical_history_label = CTkLabel(pt_details_pt_view,text="Medical History:")
+    patient_weight_label = CTkLabel(pt_details_pt_view,text="Recent Weight:")
+    patient_height_label = CTkLabel(pt_details_pt_view,text="Recent Height:")
+    patient_bmi_label = CTkLabel(pt_details_pt_view,text="Recent BMI:")
+    patient_allergies_label = CTkLabel(pt_details_pt_view,text="Known Allergies:")
+    patient_contact_label = CTkLabel(pt_details_pt_view,text="Patient Contact Num.:")
+    patient_emer_contact_label = CTkLabel(pt_details_pt_view,text="Patient Emergency Contact Num.:")
 
     #setting grid position for labels
-    welcome_text.grid(row=0,column=0,columnspan=3)
-    ic_label.grid(row=2,column=0)
-    blood_pressure_label.grid(row=3,column=0)
-    spiro_oxy_label.grid(row=4,column=0)
-    heart_rate_label.grid(row=5,column=0)
-    temperature_label.grid(row=6,column=0)
-    current_disease_label.grid(row=7,column=0)
-    medical_history_label.grid(row=8,column=0)
-    patient_weight_label.grid(row=9,column=0)
-    patient_height_label.grid(row=10,column=0)
-    patient_bmi_label.grid(row=11,column=0)
-    patient_allergies_label.grid(row=12,column=0)
-    patient_contact_label.grid(row=13,column=0)
-    patient_emer_contact_label.grid(row=14,column=0)
+    welcome_text.grid(row=0,column=0,columnspan=2,pady=80)
+    ic_label.grid(row=2,column=0,sticky="w",padx=30)
+    blood_pressure_label.grid(row=3,column=0,sticky="w",padx=30)
+    spiro_oxy_label.grid(row=4,column=0,sticky="w",padx=30)
+    heart_rate_label.grid(row=5,column=0,sticky="w",padx=30)
+    temperature_label.grid(row=6,column=0,sticky="w",padx=30)
+    current_disease_label.grid(row=7,column=0,sticky="w",padx=30)
+    medical_history_label.grid(row=8,column=0,sticky="w",padx=30)
+    patient_weight_label.grid(row=9,column=0,sticky="w",padx=30)
+    patient_height_label.grid(row=10,column=0,sticky="w",padx=30)
+    patient_bmi_label.grid(row=11,column=0,sticky="w",padx=30)
+    patient_allergies_label.grid(row=12,column=0,sticky="w",padx=30)
+    patient_contact_label.grid(row=13,column=0,sticky="w",padx=30)
+    patient_emer_contact_label.grid(row=14,column=0,sticky="w",padx=30)
 
     #creating labels for output
-    ic_output = Label(pt_details_pt_view,text=username)
-    blood_pressure_output= Label(pt_details_pt_view,text=blood_pressure)
-    spiro_oxy_output= Label(pt_details_pt_view,text=spiro_oxy)
-    heart_rate_output= Label(pt_details_pt_view,text=heart_rate)
-    temperature_output= Label(pt_details_pt_view,text=temperature)
-    current_disease_output= Label(pt_details_pt_view,text=current_disease)
-    medical_history_output= Label(pt_details_pt_view,text=medical_history)
-    patient_weight_output= Label(pt_details_pt_view,text=patient_weight)
-    patient_height_output= Label(pt_details_pt_view,text=patient_height)
-    patient_bmi_output= Label(pt_details_pt_view,text=patient_bmi)
-    patient_allergies_output= Label(pt_details_pt_view,text=patient_allergies)
-    patient_contact_output= Label(pt_details_pt_view,text=patient_contact)
-    patient_emer_contact_output= Label(pt_details_pt_view,text=patient_emer_contact)
+    ic_output = CTkLabel(pt_details_pt_view,text=username)
+    blood_pressure_output= CTkLabel(pt_details_pt_view,text=blood_pressure)
+    spiro_oxy_output= CTkLabel(pt_details_pt_view,text=spiro_oxy)
+    heart_rate_output= CTkLabel(pt_details_pt_view,text=heart_rate)
+    temperature_output= CTkLabel(pt_details_pt_view,text=temperature)
+    current_disease_output= CTkLabel(pt_details_pt_view,text=current_disease)
+    medical_history_output= CTkLabel(pt_details_pt_view,text=medical_history)
+    patient_weight_output= CTkLabel(pt_details_pt_view,text=patient_weight)
+    patient_height_output= CTkLabel(pt_details_pt_view,text=patient_height)
+    patient_bmi_output= CTkLabel(pt_details_pt_view,text=patient_bmi)
+    patient_allergies_output= CTkLabel(pt_details_pt_view,text=patient_allergies)
+    patient_contact_output= CTkLabel(pt_details_pt_view,text=patient_contact)
+    patient_emer_contact_output= CTkLabel(pt_details_pt_view,text=patient_emer_contact)
     
-    ic_output.grid(row=2,column=2)
-    blood_pressure_output.grid(row=3,column=2)
-    spiro_oxy_output.grid(row=4,column=2)
-    heart_rate_output.grid(row=5,column=2)
-    temperature_output.grid(row=6,column=2)
-    current_disease_output.grid(row=7,column=2)
-    medical_history_output.grid(row=8,column=2)
-    patient_weight_output.grid(row=9,column=2)
-    patient_height_output.grid(row=10,column=2)
-    patient_bmi_output.grid(row=11,column=2)
-    patient_allergies_output.grid(row=12,column=2)
-    patient_contact_output.grid(row=13,column=2)
-    patient_emer_contact_output.grid(row=14,column=2)
+    ic_output.grid(row=2,column=1,sticky="w",padx=30)
+    blood_pressure_output.grid(row=3,column=1,sticky="w",padx=30)
+    spiro_oxy_output.grid(row=4,column=1,sticky="w",padx=30)
+    heart_rate_output.grid(row=5,column=1,sticky="w",padx=30)
+    temperature_output.grid(row=6,column=1,sticky="w",padx=30)
+    current_disease_output.grid(row=7,column=1,sticky="w",padx=30)
+    medical_history_output.grid(row=8,column=1,sticky="w",padx=30)
+    patient_weight_output.grid(row=9,column=1,sticky="w",padx=30)
+    patient_height_output.grid(row=10,column=1,sticky="w",padx=30)
+    patient_bmi_output.grid(row=11,column=1,sticky="w",padx=30)
+    patient_allergies_output.grid(row=12,column=1,sticky="w",padx=30)
+    patient_contact_output.grid(row=13,column=1,sticky="w",padx=30)
+    patient_emer_contact_output.grid(row=14,column=1,sticky="w",padx=30)
 
     def john():
         pt_details_pt_view.destroy()
         
     #Close button
-    close_button_pt_details_pt_view = Button(pt_details_pt_view,text="Close",command=john,width=10).grid(row=15,column=1)
+    close_button_pt_details_pt_view = CTkButton(pt_details_pt_view,text="Close",command=john,fg_color="red").grid(row=15,column=0,columnspan =2,ipadx=100,ipady=10,padx=30,pady=40)
 
 #Function for querying staff full name
 def staff_details_collect(username):
@@ -158,30 +159,30 @@ def patient_check_right_wrong(pt_username):
     
 #Staff View    
 def pt_details_staff_view(root_home,username):
-    pt_details_staff_view = Toplevel(root_home)
+    pt_details_staff_view = CTkToplevel(root_home)
     pt_details_staff_view.title("MCB: Staff View")
-    pt_details_staff_view.geometry("430x350")               
+    pt_details_staff_view.geometry("1280x720")               
     
     #Search bar for patient's IC
-    search_bar = Entry(pt_details_staff_view)
+    search_bar = CTkEntry(pt_details_staff_view)
     search_bar.grid(row=1, column=0, columnspan=2)
     search_bar.insert(0,"Search Patient via I.C.")
     
     #creating labels
-    welcome_text= Label(pt_details_staff_view, font=("Times",12), text="Search for patient details, "+staff_details_collect(username))
-    name_label = Label(pt_details_staff_view,text="Patient's name:")
-    blood_pressure_label = Label(pt_details_staff_view,text="Blood Pressure:")
-    spiro_oxy_label = Label(pt_details_staff_view,text="SpO2:")
-    heart_rate_label = Label(pt_details_staff_view,text="Heart Rate:")
-    temperature_label = Label(pt_details_staff_view,text="Body Temperature:")
-    current_disease_label = Label(pt_details_staff_view,text="Current Diseases:")
-    medical_history_label = Label(pt_details_staff_view,text="Medical History:")
-    patient_weight_label = Label(pt_details_staff_view,text="Recent Weight:")
-    patient_height_label = Label(pt_details_staff_view,text="Recent Height:")
-    patient_bmi_label = Label(pt_details_staff_view,text="Recent BMI:")
-    patient_allergies_label = Label(pt_details_staff_view,text="Known Allergies:")
-    patient_contact_label = Label(pt_details_staff_view,text="Patient Contact Num.:")
-    patient_emer_contact_label = Label(pt_details_staff_view,text="Patient Emergency Contact Num.:")
+    welcome_text= CTkLabel(pt_details_staff_view, font=("Times",12), text="Search for patient details, "+staff_details_collect(username))
+    name_label = CTkLabel(pt_details_staff_view,text="Patient's name:")
+    blood_pressure_label = CTkLabel(pt_details_staff_view,text="Blood Pressure:")
+    spiro_oxy_label = CTkLabel(pt_details_staff_view,text="SpO2:")
+    heart_rate_label = CTkLabel(pt_details_staff_view,text="Heart Rate:")
+    temperature_label = CTkLabel(pt_details_staff_view,text="Body Temperature:")
+    current_disease_label = CTkLabel(pt_details_staff_view,text="Current Diseases:")
+    medical_history_label = CTkLabel(pt_details_staff_view,text="Medical History:")
+    patient_weight_label = CTkLabel(pt_details_staff_view,text="Recent Weight:")
+    patient_height_label = CTkLabel(pt_details_staff_view,text="Recent Height:")
+    patient_bmi_label = CTkLabel(pt_details_staff_view,text="Recent BMI:")
+    patient_allergies_label = CTkLabel(pt_details_staff_view,text="Known Allergies:")
+    patient_contact_label = CTkLabel(pt_details_staff_view,text="Patient Contact Num.:")
+    patient_emer_contact_label = CTkLabel(pt_details_staff_view,text="Patient Emergency Contact Num.:")
 
     #setting grid position for labels
     welcome_text.grid(row=0,column=0,columnspan=3)
@@ -200,19 +201,19 @@ def pt_details_staff_view(root_home,username):
     patient_emer_contact_label.grid(row=14,column=0)
 
     #creating labels for output
-    name_output = Entry(pt_details_staff_view)
-    blood_pressure_output= Entry(pt_details_staff_view)
-    spiro_oxy_output= Entry(pt_details_staff_view)
-    heart_rate_output= Entry(pt_details_staff_view)
-    temperature_output= Entry(pt_details_staff_view)
-    current_disease_output= Entry(pt_details_staff_view)
-    medical_history_output= Entry(pt_details_staff_view)
-    patient_weight_output= Entry(pt_details_staff_view)
-    patient_height_output= Entry(pt_details_staff_view)
-    patient_bmi_output= Entry(pt_details_staff_view)
-    patient_allergies_output= Entry(pt_details_staff_view)
-    patient_contact_output= Entry(pt_details_staff_view)
-    patient_emer_contact_output= Entry(pt_details_staff_view)
+    name_output = CTkEntry(pt_details_staff_view)
+    blood_pressure_output= CTkEntry(pt_details_staff_view)
+    spiro_oxy_output= CTkEntry(pt_details_staff_view)
+    heart_rate_output= CTkEntry(pt_details_staff_view)
+    temperature_output= CTkEntry(pt_details_staff_view)
+    current_disease_output= CTkEntry(pt_details_staff_view)
+    medical_history_output= CTkEntry(pt_details_staff_view)
+    patient_weight_output= CTkEntry(pt_details_staff_view)
+    patient_height_output= CTkEntry(pt_details_staff_view)
+    patient_bmi_output= CTkEntry(pt_details_staff_view)
+    patient_allergies_output= CTkEntry(pt_details_staff_view)
+    patient_contact_output= CTkEntry(pt_details_staff_view)
+    patient_emer_contact_output= CTkEntry(pt_details_staff_view)
     
     #Position button grid
     name_output.grid(row=2,column=2)
@@ -298,8 +299,8 @@ def pt_details_staff_view(root_home,username):
         pt_details_staff_view.destroy()
        
     #Retrieve Patient Data
-    retrieve_patient_data_button = Button(pt_details_staff_view, text="Retrieve",command=retrieve_patient_data,width=10).grid(row=1,column=2)
+    retrieve_patient_data_button = CTkButton(pt_details_staff_view, text="Retrieve",command=retrieve_patient_data,width=10).grid(row=1,column=2)
     #Update Patient Data
-    update_patient_data_button = Button(pt_details_staff_view,text="Update",command=update_patient_data).grid(row=16,column=1)
+    update_patient_data_button = CTkButton(pt_details_staff_view,text="Update",command=update_patient_data).grid(row=16,column=1)
     #Close button
-    close_button_pt_details_staff_view = Button(pt_details_staff_view,text="Close",command=john,width=10).grid(row=16,column=3)
+    close_button_pt_details_staff_view = CTkButton(pt_details_staff_view,text="Close",command=john,width=10).grid(row=16,column=3)
